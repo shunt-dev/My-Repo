9c80e91a0f14f0649add78e8e4856737eef5e019
shunt-dev <shantanu.amberkar100@gmail.com>
2026-03-17 11:06:20 +0530

    Update bubble.c
    Changed swapping from 3 to 2 variables

--- bubble.c
+++ bubble.c
@@ -12,9 +12,9 @@ int main(){
         {
             if(arr[j]<arr[j+1])
             {
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                arr[j]=arr[j]+arr[j+1];
+                arr[j+1]=arr[j]-arr[j+1];
+                arr[j]=arr[j]-arr[j+1];
             }
         }
     }
